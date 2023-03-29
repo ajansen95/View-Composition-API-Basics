5179604b38b4a7427b13b84164bada000419c5a9
Alexander Jansen <126659284+ajansen95@users.noreply.github.com>
2023-03-29 14:23:36 +0200

    Abschnitt 4: 18. Non-Reactive Data (done)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="home">
 
+        <h2>{{appTitle}}</h2>
+
         <h3>{{counterData.title}}:</h3>
 
         <div>
@@ -20,6 +22,8 @@
 
 <script setup>
 import {reactive} from "vue";
+
+const appTitle = 'My Ok Counter App'
 
 const counterData = reactive({
     count: 0,
