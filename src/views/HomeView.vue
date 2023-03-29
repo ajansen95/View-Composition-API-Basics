6459c2a804a4d8a75b5c87d67a653e8e5bc0a07c
Alexander Jansen <126659284+ajansen95@users.noreply.github.com>
2023-03-29 14:10:24 +0200

    Abschnitt 4: Refs, Reactive Objects & Non-Reactive Data, Video 16 (done)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,31 @@
 <template>
     <div class="home">
 
+        <h3>{{counterTitle}}:</h3>
+
         <div>
             <button @click="decreaseCounter" class="btn">-</button>
             <span class="counter">{{counter}}</span>
             <button @click="increaseCounter" class="btn">+</button>
         </div>
 
+        <div class="edit">
+            <h4>Edit counter title:</h4>
+            <input v-model="counterTitle" type="text">
+        </div>
+
     </div>
 </template>
+
+
+<script setup>
+import {ref} from "vue";
+
+const counter = ref(0)
+const counterTitle = ref('My Counter')
+function increaseCounter() {counter.value++}
+const decreaseCounter = () => {counter.value--}
+</script>
 
 
 <style>
@@ -20,50 +37,7 @@
     font-size: 40px;
     margin: 10px;
 }
+.edit {
+    margin-top: 60px;
+}
 </style>
-
-
-<script setup>
-import {ref} from "vue";
-
-const counter = ref(0)
-function increaseCounter() {counter.value++}
-const decreaseCounter = () => {counter.value--}
-</script>
-
-
-<!--
-<script>
-import {ref} from "vue";
-export default {
-    setup() {
-        const counter = ref(0)
-        function increaseCounter() {counter.value++}
-        const decreaseCounter = () => {counter.value--}
-
-        return {counter, increaseCounter, decreaseCounter}
-    }
-}
-</script>
--->
-
-
-<!--
-<script>
-export default {
-    data() {
-        return {
-            counter: 0
-        }
-    },
-    methods: {
-        increaseCounter() {
-            this.counter++
-        },
-        decreaseCounter() {
-            this.counter--
-        }
-    }
-}
-</script>
--->
