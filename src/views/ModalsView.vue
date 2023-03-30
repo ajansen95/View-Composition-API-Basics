d4ba32b4b3ea3c3fac0cb8957c7a9d796c96a5cb
Alexander Jansen <126659284+ajansen95@users.noreply.github.com>
2023-03-30 13:25:57 +0200

    Abschnitt 9: Lists, Teleport, Template Refs & nextTick, 42. Teleport - Part 2 (done)

--- src/views/ModalsView.vue
+++ src/views/ModalsView.vue
@@ -2,15 +2,14 @@
   <div class="modals">
       <h1>Modals</h1>
       <button @click="showModal = true">Show Modal</button>
-      <div
-          class="modal"
-          v-if="showModal"
-      >
-          <h1>This is a modal</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda autem ex laboriosam sequi voluptates. A
-              ab inventore nobis. A alias aliquam dolor dolore ea eaque nihil omnis quis reiciendis tempore.</p>
-          <button @click="showModal = false">Hide Modal</button>
-      </div>
+      <teleport to=".modals-container">
+          <div class="modal" v-if="showModal">
+              <h1>This is a modal</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda autem ex laboriosam sequi voluptates. A
+                  ab inventore nobis. A alias aliquam dolor dolore ea eaque nihil omnis quis reiciendis tempore.</p>
+              <button @click="showModal = false">Hide Modal</button>
+          </div>
+      </teleport>
   </div>
 </template>
 
@@ -34,5 +33,11 @@
     background: beige;
     padding: 10px;
     color: #282828;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1000;
 }
 </style>
